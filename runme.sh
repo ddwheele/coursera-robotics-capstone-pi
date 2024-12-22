dac9ac44ee9c1316472b638fce782557c01cc0ed
DW Wheeler <dawn.wheeler@gmail.com>
2024-12-21 20:42:47 -0800

    add a runme.sh script to start the Docker container

---
+++ runme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script runs a Docker container, bind mounting ~/.ssh for git interaction,
+# and mounting a Volume which has the repository checked out into it.
+
+echo "Running the container..."
+
+sudo docker run -it --rm \
+	--mount type=bind,src=/home/dw/.ssh,dst=/root/.ssh \
+	--mount type=volume,src=capstone,dst=/root/capstone \
+       	ros-desktop
