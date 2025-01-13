017b46881f244b618887666deb9514ffbca18fee
DW Wheeler <dw.wheeler@nasa.gov>
2025-01-12 21:07:28 -0800

    cleanup

--- SimulatorCode/mathUtils.py
+++ SimulatorCode/mathUtils.py
@@ -120,9 +120,6 @@ def tag_in_world_to_tag_in_camera(cam, tag) :
     sy = cam[1] + np.sin(cam_theta - np.pi/2) 
 
     # slope of line perpendicular to camera Z axis
-    if math.isclose(sx - cam[0],0):
-      print("this shouldn't happen")
-
     m = (sy - cam[1]) / (sx - cam[0])
 
     # the sign of this value indicates which side of the line the camera can see
@@ -137,102 +134,7 @@ def tag_in_world_to_tag_in_camera(cam, tag) :
       relative_theta = cant_see_it
 
   # cP is in fake camera coords, so need to swap order to get x, z as expected
-  ans = [cP[1], cP[0], relative_theta]
-
   return [cP[1], cP[0], relative_theta]
-
-
-# Input:
-#   cam: camera position in world coordinates (x, y, theta = view axis)
-#   tag: tag position in world coordinates (x,y, theta = out back of tag)
-#   world coordinates are x = right, y = forward, theta around vertical z (right-handed)
-# Output:
-#   tag position in camera coordinates (x,z,theta = 0 if straight on, +ive if rotated around vertical) 
-#   camera coordinates are x = left, z = forward = view axis (left-handed)
-def tag_in_world_to_tag_in_camera_old(cam, tag):
-  cant_see_it = 5 # angle > pi/2, indicates tag is not visible in camera
-  camx = cam[0]
-  camy = cam[1]
-  cam_theta = ensure_between( cam[2], -np.pi, np.pi)
-
-  tagx = tag[0]
-  tagy = tag[1]
-  tag_theta = ensure_between(tag[2], 0, 2*np.pi) 
-  
-  ct = np.cos(cam_theta)
-  st = np.sin(cam_theta)
-  
-  rotmat = np.array([[ct, st, 0], [-st, ct, 0], [0,0,1]],dtype=object)
-
-  # transformed origin offset
-  cam_origin = np.array([camx, camy, 1],dtype=object)
-  transformed_origin = np.matmul(rotmat, cam_origin)
-  
-  tag_world = np.array([tag[0], tag[1], 1])    
-  tag_camera = np.matmul(rotmat, tag_world)
-
-  relative_theta = 0
-
-  # take care of positions where lines are vertical/horizontal
-  if math.isclose(cam_theta,0): # cam points right
-    if tagx >= camx: # tag is to right of cam
-      if tag_theta >= -np.pi/2 and tag_theta <= np.pi/2: # tag points generally right
-        relative_theta = tag_theta 
-      else: # tag too oblique
-        relative_theta = cant_see_it
-    else: # tag is to left of cam
-      relative_theta = cant_see_it
-  elif math.isclose(cam_theta, np.pi/2): # cam points forward
-    if tagy >= camy: # tag is forward of camera
-      if tag_theta >= 0 and tag_theta < np.pi: # tag points generally forward
-        relative_theta =  ensure_between( tag_theta-np.pi/2, -np.pi, np.pi)# rotate by 90 degrees
-      else: # tag too oblique
-        relative_theta = cant_see_it
-    else: # tag behind cam
-      relative_theta = cant_see_it
-  elif math.isclose(cam_theta, np.pi): # cam points left
-    if tagx <= camx: # tag is left of cam
-      if tag_theta >= np.pi/2 and tag_theta <= 3*np.pi/2: # tag points left-ish
-        relative_theta = ensure_between(tag_theta+np.pi, -np.pi, np.pi)# rotate by 180 degrees
-      else: # tag too oblique
-        relative_theta = cant_see_it
-    else: # tag behind cam
-      relative_theta = cant_see_it
-  elif math.isclose(cam_theta, -np.pi/2): # cam points -y
-    if tagy <= camy: # tag is -y of cam
-      if tag_theta >= np.pi: # tag points -y-ish
-        relative_theta = ensure_between(tag_theta-np.pi/2, -np.pi, np.pi)# rotate by 90 degrees
-      elif tag_theta == 0: # because 0 = 2*pi
-        relative_theta = ensure_between(tag_theta+np.pi/2, -np.pi, np.pi)# rotate by 90 degrees
-      else: # tag too oblique
-        relative_theta = cant_see_it
-    else: # tag behind cam
-      relative_theta = cant_see_it
-
-  else: # no gimbal lock
-    # zx, zy is directly in front of camera
-    zx = camx + np.cos(cam_theta)
-    zy = camy + np.sin(cam_theta) 
-
-    # sx, sy is directly to the right of the camera
-    sx = camx + np.cos(cam_theta - np.pi/2)
-    sy = camy + np.sin(cam_theta - np.pi/2) 
-
-    # slope of line perpendicular to camera Z axis
-    m = (sy - camy) / (sx - camx)
-
-    # the sign of this value indicates which side of the line the camera can see
-    visible = m*(zx - camx) - (zy - camy)
-
-    # the sign of this value indicates which side of the line the tag is on
-    tag_side = m*(tagx - camx) - (tagy - camy)
-
-    if math.isclose(tag_side,0) or (visible>0 and tag_side>0) or (visible<0 and tag_side<0):
-      relative_theta = ensure_between(ensure_between(tag_theta,-np.pi,np.pi) - cam_theta,-np.pi,np.pi)
-    else:
-      relative_theta = cant_see_it
-
-  return [tag_camera[1]-transformed_origin[1], tag_camera[0]-transformed_origin[0], relative_theta]
 
 # Input:
 #   rob: robot position in world coordinates (x,y,theta)
